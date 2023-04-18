beaf5d49e43fee100327d506250bcbf54f13f811
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-04-18 23:17:00 +0530

    Sign in screen

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,39 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: const Text('Sign in to Brew Crew'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+        child: ElevatedButton(
+          child: const Text('Sign in'),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('Error in Signing in');
+            } else {
+              print('Signed in');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
