f04062cf72996a0606060f27a660662473654b93
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-04-18 22:57:40 +0530

    auth sign in  anonymously

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,9 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
- 
+
 class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   //sign in anonymous
+  Future signInAnon() async {
+    try {
+      UserCredential userCred = await _auth.signInAnonymously();
+      User? user = userCred.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
   // sign in with email
   // register
   // sign out
-
-} 
+}
