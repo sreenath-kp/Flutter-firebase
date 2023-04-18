533f36b63c5add172f1d6f2420303d4e85cd6100
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-04-18 22:37:40 +0530

    first commit

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Return either home or authenticate
+    return Authenticate();
+  }
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+ 
+class AuthService {
+  //sign in anonymous
+  // sign in with email
+  // register
+  // sign out
+
+} 
